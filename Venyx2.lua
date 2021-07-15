4249e09402e1bc7bdd01c07b6e4ff00ad911f349
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2021-07-15 17:22:56 -0400

    Update Venyx2.lua

--- Venyx2.lua
+++ Venyx2.lua
@@ -222,7 +222,7 @@ do
 	function library.new(title)
 		local container = utility:Create("ScreenGui", {
 			Name = title,
-			Parent = player.PlayerGui--game.CoreGui
+			Parent = game:GetService("CoreGui")
 		}, {
 			utility:Create("ImageLabel", {
 				Name = "Main",
