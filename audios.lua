80f888231912667af8933b5fdd4b314de702ca56
iamtryingtofindname <70244612+iamtryingtofindname@users.noreply.github.com>
2021-10-02 14:14:11 -0400

    Created

---
+++ audios.lua
@@ -0,0 +1,40 @@
+local audios = {
+	{
+		ID = 4524572739;
+		Name = "Gunna - Drip Too Hard (30s)"
+	};
+    {
+		ID = 1023116114;
+		Name = "I'm Mr. Meeseeks!"
+	};
+    {
+		ID = 850994651;
+		Name = "You're Welcome (Rick and Morty)"
+	};
+    {
+		ID = 3043669806;
+		Name = "Rick and Morty Intro"
+	};
+    {
+		ID = 6817255053;
+		Name = "Lil Mosey - Noticed"
+	};
+    {
+		ID = 3180460921;
+		Name = "Old Town Road OOFED"
+	};
+    {
+		ID = 3478313680;
+		Name = "Juice WRLD - Lucid Dreams"
+	};
+    {
+		ID = 2590490779;
+		Name = "Crab Rave OOFED"
+	};
+    {
+		ID = 261605308;
+		Name = "Souja Boy - Rick and Morty"
+	};
+}
+-- i could have used JSON decoding, but I was lazy
+shared.audios = audios
